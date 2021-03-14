2cc27f9c6fb39333733e417df294c107bf2b9326
JCNextCode <jae.jc.cho@gmail.com>
2021-03-13 23:39:57 -0500

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Jae Cho on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
